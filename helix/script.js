527f9e772390364881578c6365b4fe4c20a8cd69
4ngel#2769 <angel.helix123@gmail.com>
2021-12-30 04:52:12 -0500

    new helix website

---
+++ helix/script.js
@@ -0,0 +1,33 @@
+const nounou=[
+    'location',
+    'href',
+    'querySelector',
+    'https://discord.com/oauth2/authorize?scope=bot+applications.commands&client_id=723697439638290482&permissions=534722768247',
+    'Made by @angel_capra | Copyright © 2020-2021',
+    'createTextNode',
+    'button.s-m-btn',
+    'createElement',
+    'footer',
+    'onclick',
+    'button.hero-btn',
+    'appendChild'
+];
+
+(function(Nounou,nOunou){
+    const NOunou=function(nOUnou){
+        while(--nOUnou){
+            Nounou['push'](Nounou['shift']());
+        }
+    };
+    NOunou(++nOunou);
+}
+(nounou,0xfd));
+const Nounou=function(nOunou,NOunou){
+    nOunou=nOunou-0x0;
+    let noUnou=nounou[nOunou];
+    return noUnou;
+};
+document[Nounou('0x1')](Nounou('0x9'))[Nounou('0x8')]=nOunou=>window[Nounou('0xb')][Nounou('0x0')]=Nounou('0x2');
+document[Nounou('0x1')](Nounou('0x5'))[Nounou('0x8')]=NOunou=>window[Nounou('0xb')][Nounou('0x0')]='https://discord.com/oauth2/authorize?scope=bot+applications.commands&client_id=723697439638290482&permissions=534722768247';
+let footer=document[Nounou('0x6')](Nounou('0x7'));
+let text=document[Nounou('0x4')](Nounou('0x3'));footer[Nounou('0xa')](text);document['body'][Nounou('0xa')](footer);
