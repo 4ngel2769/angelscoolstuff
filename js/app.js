11dfb200d284df9cbcf27bd0af586e86c531b024
Angel C <angel.helix123@gmail.com>
2023-12-04 15:09:30 +0200

    Update app.js

--- js/app.js
+++ js/app.js
@@ -11,63 +11,116 @@ particlesJS.load('particles-js', 'particles.json', function() {
 
 /* Otherwise just put the config content (json): */
 
-particlesJS("particles-js", {
-  particles: {
-    number: {
-      value: 90,
-      density: { enable: true, value_area: 631.3480069132609 }
+particlesJS("particles-js",
+    {
+  "particles": {
+    "number": {
+      "value": 57,
+      "density": {
+        "enable": true,
+        "value_area": 551.3981882630954
+      }
     },
-    color: { value: "#000000" }, // #cdcdcd
-    shape: {
-      type: "circle",
-      stroke: { width: 0, color: "#000000" },
-      polygon: { nb_sides: 6 },
-      image: { src: "img/github.svg", width: 100, height: 100 }
+    "color": {
+      "value": "#33d1ff"
     },
-    opacity: {
-      value: 0.5,
-      random: false,
-      anim: { enable: true, speed: 1, opacity_min: 0.1, sync: false }
+    "shape": {
+      "type": "circle",
+      "stroke": {
+        "width": 0,
+        "color": "#000000"
+      },
+      "polygon": {
+        "nb_sides": 5
+      },
+      "image": {
+        "src": "img/github.svg",
+        "width": 100,
+        "height": 100
+      }
     },
-    size: {
-      value: 4,
-      random: true,
-      anim: { enable: false, speed: 40, size_min: 0.1, sync: false }
+    "opacity": {
+      "value": 0.5,
+      "random": false,
+      "anim": {
+        "enable": false,
+        "speed": 1,
+        "opacity_min": 0.1,
+        "sync": false
+      }
     },
-    line_linked: {
-      enable: true,
-      distance: 192.28766234286476,
-      color: "#3dfcff",
-      opacity: 0.29644347944524985,
-      width: 0
+    "size": {
+      "value": 3,
+      "random": true,
+      "anim": {
+        "enable": false,
+        "speed": 40,
+        "size_min": 0.1,
+        "sync": false
+      }
     },
-    move: {
-      enable: true,
-      speed: 6,
-      direction: "none",
-      random: true,
-      straight: false,
-      out_mode: "bounce",
-      bounce: false,
-      attract: { enable: false, rotateX: 600, rotateY: 1200 }
+    "line_linked": {
+      "enable": true,
+      "distance": 143.94242303078775,
+      "color": "#a9f5ff",
+      "opacity": 0.2718912435025991,
+      "width": 0.959616153538585
+    },
+    "move": {
+      "enable": true,
+      "speed": 3.1508467900748314,
+      "direction": "none",
+      "random": true,
+      "straight": false,
+      "out_mode": "out",
+      "bounce": false,
+      "attract": {
+        "enable": false,
+        "rotateX": 787.7116975187079,
+        "rotateY": 1200
+      }
     }
   },
-  interactivity: {
-    detect_on: "canvas",
-    events: {
-      onhover: { enable: true, mode: "grab" },
-      onclick: { enable: true, mode: "push" },
-      resize: true
+  "interactivity": {
+    "detect_on": "canvas",
+    "events": {
+      "onhover": {
+        "enable": false,
+        "mode": "repulse"
+      },
+      "onclick": {
+        "enable": false,
+        "mode": "push"
+      },
+      "resize": true
     },
-    modes: {
-      grab: { distance: 251.7482517482518, line_linked: { opacity: 1 } },
-      bubble: { distance: 400, size: 40, duration: 2, opacity: 8, speed: 3 },
-      repulse: { distance: 200, duration: 0.4 },
-      push: { particles_nb: 4 },
-      remove: { particles_nb: 2 }
+    "modes": {
+      "grab": {
+        "distance": 400,
+        "line_linked": {
+          "opacity": 1
+        }
+      },
+      "bubble": {
+        "distance": 400,
+        "size": 40,
+        "duration": 2,
+        "opacity": 8,
+        "speed": 3
+      },
+      "repulse": {
+        "distance": 200,
+        "duration": 0.4
+      },
+      "push": {
+        "particles_nb": 4
+      },
+      "remove": {
+        "particles_nb": 2
+      }
     }
   },
-  retina_detect: true
+  "retina_detect": true
 });
       //"background_color": "#b61924",
       //"background_image": "",
